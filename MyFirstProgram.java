2be7626abd473213e89831ca0d6a3f5edb556f0c
Deep Inside <vanya0759@gmail.com>
2021-10-26 14:02:22 +0300

    first commit

---
+++ MyFirstProgram.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args){
+		System.out.println("kekli i lulzi");
+		Scanner scan = new Scanner(System.in);
+		String name = scan.nextLine();
+		System.out.println("putin huilo " + name);
+		
+	}
+
+}
